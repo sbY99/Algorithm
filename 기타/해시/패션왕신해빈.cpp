7629820b2df0241164a11fec98acf658f2a4c629
sbY99 <didtmdqlsdl@gmail.com>
2022-02-02 00:03:06 +0900

    "

--- 기타/해시/패션왕신해빈.cpp
+++ 기타/해시/패션왕신해빈.cpp
@@ -12,12 +12,21 @@ int main()
     {
         cin >> N;
         map<string, int> M;
+
         for (int j = 0; j < N; j++)
         {
             string name, type;
             cin >> name >> type;
             M[type]++;
         }
+
+        int count = 1;
+        map<string, int>::iterator iter;
+        for (iter = M.begin(); iter != M.end(); iter++)
+        {
+            count *= ((iter->second) + 1);
+        }
+        printf("%d\n", count - 1);
     }
 
     return 0;
